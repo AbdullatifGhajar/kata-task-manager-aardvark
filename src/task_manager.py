4781f27ad8f1e61934d2762caf1f04c848dfeabc
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-15 10:42:25 +0100

    Add task manager
    Co-authored-by: Malo Le Corvec <malo.le.corvec@efrei.net>
    Co-authored-by: Biily-Gagnaire Baptiste <ashwabbg@users.noreply.github.com>

---
+++ src/task_manager.py
@@ -0,0 +1,16 @@
+class TaskManager:
+    def __init__(self):
+        self.list = []
+        
+    def add_task(self, description: str):
+        pass
+    
+    def remove_task(self, id: int):
+        pass
+    
+    def set_done(self, id: int):
+        pass
+    
+    def set_todo(self, id: int):
+        pass
+
