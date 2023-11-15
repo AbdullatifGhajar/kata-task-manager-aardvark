ea816af8c4cbcf98606724ac6001d106969e251c
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-15 11:15:26 +0100

    Add counter

--- src/todo_list.py
+++ src/todo_list.py
@@ -3,6 +3,8 @@ from src.task import Task
 class TodoList:
     def __init__(self):
         self.tasks = {}
+        self.counter = 1
         
     def add_task(self, description:str):
-        self.tasks[1] = Task(description)
+        self.tasks[self.counter] = Task(description)
+        self.counter += 1
