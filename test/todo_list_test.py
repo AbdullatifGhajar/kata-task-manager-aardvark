64435c01ef81cb98720478cb26d0fdb53136f7d3
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-15 11:08:05 +0100

    Add test for add_task

--- test/todo_list_test.py
+++ test/todo_list_test.py
@@ -12,3 +12,11 @@ def test_has_tasks_as_dict():
     todo_list = TodoList()
     
     assert todo_list.tasks == {}
+    
+def test_add_task():
+    todo_list = TodoList()
+    
+    task_description = "Do the dishes"
+    todo_list.add_task(task_description)
+    
+    assert todo_list.tasks == {1: Task(task_description)}
